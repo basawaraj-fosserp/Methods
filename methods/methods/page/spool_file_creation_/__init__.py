84e4cbe2315bb34350337e438529b82dd4e00967
viralkansodiya <viral@fosserp.com>
2024-10-01 09:59:39 +0000

    ch

---
+++ methods/methods/page/spool_file_creation_/__init__.py
@@ -0,0 +1,80 @@
+import frappe
+from datetime import datetime
+import ast
+from frappe.utils import getdate, get_url
+from frappe.model.naming import make_autoname
+
+
+@frappe.whitelist()
+def get_sales_invoice(from_date, to_date):
+    condition = ''
+    if from_date:
+        condition += f" and si.posting_date >= '{from_date}'"
+    if to_date:
+        condition += f" and si.posting_date <= '{to_date}'"
+
+    data = frappe.db.sql(f"""
+            Select si.name, 
+            si.posting_date, 
+            si.customer, 
+            si.customer_name,
+            si.irn, 
+            si.schedule_no, 
+            si.po_no, 
+            si.company_gstin
+            From `tabSales Invoice` as si
+            Where si.docstatus=1 and si.customer_name like '%Maruti Suzuki%' {condition} 
+    """, as_dict=1 )
+
+    for row in data:
+        row.update({"vendor_no" : "M061", "spares_indicator" : "S"})
+        row.update({"posting_date" : row.posting_date.strftime('%d-%m-%Y')})
+    return data
+
+
+import frappe
+import ast
+from frappe.utils import get_files_path
+
+@frappe.whitelist()
+def create_spool_file(invoices):
+    actual_list = ast.literal_eval(invoices)
+    if not actual_list:
+        frappe.throw("Invoice Not Selected")
+    
+    conditions = ''
+    if actual_list:
+        conditions = " and si.name in {} ".format(
+                    "(" + ", ".join([f'"{l}"' for l in actual_list]) + ")")
+        
+    data = frappe.db.sql(f"""
+                SELECT si.name, si.schedule_no, si.posting_date, si.company_gstin, si.irn,
+                sii.item_code, sii.qty, item.per_bin_qty, item.batch_no, sii.gst_hsn_code,
+                sii.item_tax_template, sii.igst_rate, sii.igst_amount, sii.cgst_rate, sii.cgst_amount,
+                sii.sgst_rate, sii.sgst_amount
+                FROM `tabSales Invoice` AS si
+                LEFT JOIN `tabSales Invoice Item` AS sii ON sii.parent = si.name
+                LEFT JOIN `tabItem` AS item ON sii.item_code = item.name
+                WHERE si.docstatus = 1 {conditions}
+            """, as_dict=1)
+    
+    # Define the path where you want to save the file
+    file_path = get_files_path('spool.txt')  # Saves in /sites/[site_name]/public/files/spool.txt
+    
+    with open(file_path, 'w') as f:
+        for row in data:
+            gst = 0
+            if row.igst_amount:
+                gst += row.igst_amount
+            if row.cgst_amount:
+                gst += row.cgst_amount
+            if row.sgst_amount:
+                gst += row.sgst_amount
+            f.write("{0}{1}{2}{1}{2}                        {3}   {4}        {5}          {6}                                                   {7}\n".format(
+                "M061", row.name, row.posting_date.strftime('%d-%b-%Y').upper(), row.schedule_no, row.item_code, row.po_no, row.per_bin_qty, row.batch_no))
+            f.write("{0}{1}{2}                               {2}                         NA             {3}\n".format(row.company_gstin,row.gst_hsn_code ,gst,row.irn))
+
+    return file_path
+
+
+#https://methods.fosscrm.com
---
+++ methods/methods/page/spool_file_creation_/spool_file_creation_.js
@@ -0,0 +1,152 @@
+frappe.pages['spool-file-creation-'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Spool File Creation Tool',
+		single_column: true
+	});
+	frappe.SpoolDoc.init(wrapper, page)
+}
+
+
+frappe.SpoolDoc = {
+	init:function(wrapper, page){
+		this.wrapper = $(wrapper).find('.layout-main-section');
+		this.wrapper.append(`
+			<div class="invoice_data"></div>
+			`)
+		this.page = wrapper.page;
+		this.setup_fields(wrapper, page)
+		this.setup_data(wrapper, page)
+		
+	
+	},
+	setup_fields:function(wrapper, page){
+		this.from_date_field = page.add_field({
+			fieldname: 'from_date',
+			label: __('From Date'),
+			fieldtype:'Date',
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			change: function () {
+				frappe.SpoolDoc.setup_data(wrapper, page)
+			}
+		})
+		this.to_date_field = page.add_field({
+			fieldname: 'to_date',
+			label: __('To Date'),
+			fieldtype:'Date',
+			default: frappe.datetime.get_today(),
+			change: function () {
+				frappe.SpoolDoc.setup_data(wrapper, page)
+			}
+		})
+	},
+	setup_data:function(wrapper, page){
+		frappe.call({
+			 method:"methods.methods.page.spool_file_creation_.get_sales_invoice",
+			 args:{
+				'from_date': this.from_date_field.get_value(),
+				'to_date': this.from_date_field.get_value(),
+			 },
+			 callback:function(r){
+				let data = r.message
+				$(wrapper).find(".invoice_data").empty();
+				frappe.SpoolDoc.update_header(wrapper, page)
+				frappe.SpoolDoc.update_invoice_row(wrapper, page, data)
+				// Attach a change event handler to the 'selectall' checkbox
+				$('.selectall').on('change', function() {
+					// Determine if the 'selectall' checkbox is checked
+					var isChecked = $(this).is(':checked');
+					// Select all checkboxes with the ID 'method_invoice' and set their state
+					$('input[id="method_invoice"]').prop('checked', isChecked);
+				});
+				$('.create').on('click', function(){
+					frappe.SpoolDoc.create_file()
+				})
+			 }
+		})
+	},
+	update_header:function(wrapper, page){
+		this.wrapper = $(wrapper).find('.invoice_data');
+		this.wrapper.append(`
+			<style>
+				.onbottom {
+					border-bottom:2px solid black;
+				}
+				.invoice_data{
+					padding:10px;
+				}	
+				p{
+					margin-bottom:0px !important;
+				}
+				td{
+					padding:5px;
+				}
+				.row_bottom {
+					border-bottom : solid ;
+				}
+			</style>
+			
+			`)
+	},
+	update_invoice_row:function(wrapper, page, data){
+		this.wrapper = $(wrapper).find('.invoice_data');
+		let invoice_html = `<table width="100%">
+								<tr>
+									<td class="onbottom"><input type="checkbox" class="selectall"></td>
+									<td class="onbottom"><p style="font-size:16px;"><b>Invoice No</b></p></td>
+									<td class="onbottom"><p style="font-size:16px;"><b>Invoice Date</b></p></td>
+									<td class="onbottom"><p style="font-size:16px;"><b>Customer Name</b></p></td>
+								</tr>`
+		data.forEach(r => {
+			invoice_html += `<tr>
+								<td class="row_bottom"><input type="checkbox" class=${r.name} id="method_invoice"></td>
+								<td class="row_bottom">
+									<p><a href='https://methods.fosscrm.com/app/sales-invoice/${r.name}'>${r.name}</a></p>
+								</td>
+								<td class="row_bottom">
+									<p>${r.posting_date}</p>
+								</td>
+								<td class="row_bottom">
+									<p><a href='https://methods.fosscrm.com/app/customer/${r.customer}'>${r.customer_name}</a></p>
+								</td>
+							</tr>`
+			});
+		invoice_html += `</table>
+						<div width="100%" style="height:10px;"></div>	
+					`
+							
+		if(data.length){
+			invoice_html += `<div class="row">
+								<div class="col-2"><button class="btn btn-primary create">Create</button></div>
+								<div class="col-2"><button class="btn btn-primary refresh">Refresh</button></div>
+							</div>`
+			}			
+		this.wrapper.append(invoice_html)
+		
+		
+	},
+	create_file:function(){
+		let invoices = []
+		$('input[id="method_invoice"]').each(function() {
+			if ($(this).is(':checked')) {  // Check if the checkbox is checked
+				var classNames = $(this).attr('class');
+				invoices.push(classNames);  // Push the class names to the invoices array
+			}
+		});		
+		frappe.call({
+			method:"methods.methods.page.spool_file_creation_.create_spool_file",
+			args:{
+				"invoices" : invoices 
+			},
+			callback:(r)=>{
+				const fileUrl = '/files/spool.txt';  // Relative URL from your public folder
+				const a = document.createElement('a');
+				a.href = fileUrl;
+				a.download = 'spool.txt';  // Specify the file name for download
+				document.body.appendChild(a);
+				a.click();
+				document.body.removeChild(a);
+			}
+		})
+	}
+}
