3359ef1ee9985a61688ccc9a001b7350c1f6c384
viral <viral@fosserp.com>
2024-10-24 12:36:30 +0000

    ch

--- methods/methods/report/sales_v_s_stock/sales_v_s_stock.py
+++ methods/methods/report/sales_v_s_stock/sales_v_s_stock.py
@@ -3,6 +3,7 @@
 
 import frappe
 from frappe import _
+from erpnext.stock.utils import get_stock_balance
 
 def execute(filters=None):
 	columns, data = [], []
@@ -27,8 +28,8 @@ def get_sold_sle_data(filters):
 			Select  sle.warehouse, sle.item_code, sum(sle.actual_qty) * -1 as actual_qty, sum(dni.amount) as amount, dni.item_group, dni.item_name
 			From `tabStock Ledger Entry` as sle
 			Left Join `tabDelivery Note Item` as dni ON dni.parent = sle.voucher_no and sle.item_code = dni.item_code and sle.warehouse = dni.warehouse
-			Where sle.is_cancelled = 0  and sle.voucher_type in ('Delivery Note') {condition}
-			Group By sle.warehouse, sle.item_code
+			Where sle.is_cancelled = 0  and sle.voucher_type in ('Delivery Note', 'Sales Invoice') {condition}
+			Group By sle.item_code
 			Order by sle.item_code
 		""", as_dict=1)
 
@@ -37,44 +38,36 @@ def get_sold_sle_data(filters):
 		if row.warehouse not in warehouse:
 			warehouse.append(row.warehouse)
 		
-
-	condition = ''
-	if filters.get("company"):
-		condition += f" and company = '{filters.get('company')}'"
-	if filters.get("item_code"):
-		condition += f" and item_code = '{filters.get('item_code')}'"
-	if filters.get("warehouse"):
-		condition += f" and warehouse = '{filters.get('warehouse')}'"
-	if filters.get("to_date"):
-		condition += f" and posting_date <= '{filters.get('to_date')}'"
-
-	end_stock = frappe.db.sql(f"""
-			Select  warehouse, item_code, qty_after_transaction
-			From `tabStock Ledger Entry`
-			Where is_cancelled = 0 {condition}
-			Order by creation DESC
-		""", as_dict=1)
-
-	end_stock_map = {}
-
-	
-	for row in end_stock:
-		if not end_stock_map.get(row.item_code):
-			end_stock_map[row.item_code] = []
-			end_stock_map[row.item_code].append(row)
-		else:
-			end_stock_map[row.item_code].append(row)
-		
-	for row in data:
-		if end_stock_map.get(row.item_code):
-			for d in end_stock_map.get(row.item_code):
-				if row.warehouse == d.warehouse:
-					row.update({row.warehouse : d.qty_after_transaction})
-					break
-			continue
-		
+	warehouse_list = frappe.db.get_list("Warehouse", { "is_group" : 0 , "company" : filters.get("company")}, pluck="name")
 
 	columns = get_columns(filters, warehouse)
+	warehouse_column = []
+	for row in data:
+		warehouse_column.append(row.warehouse)
+		warehouse_qty = get_stock_balance(
+							row.item_code,
+							row.warehouse,
+							posting_date = filters.get("to_date")
+						)
+		row.update({ row.warehouse : warehouse_qty })
+
+		for d in warehouse_list:
+			if row.warehouse != d:
+				warehouse_qty = get_stock_balance(
+									row.item_code,
+									d,
+									posting_date = filters.get("to_date")
+								)
+				if warehouse_qty:
+					if d not in warehouse_column:
+						columns.append({
+							"fieldname" : d,
+							"label" : d,
+							"fieldtype" : "Float",
+							"width" : 150
+						})
+						warehouse_column.append(d)
+						row.update({ d : warehouse_qty })
 
 	return columns, data
 
