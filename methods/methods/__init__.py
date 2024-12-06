a351e20840c2e9a7378243117edf957fad704f37
viral <viral@fosserp.com>
2024-12-06 07:45:03 +0000

    ch

--- methods/methods/__init__.py
+++ methods/methods/__init__.py
@@ -7,5 +7,6 @@ def get_so_ref(sales_order, data):
 	name = frappe.db.get_value("Sales Order Item", {"item_code": data.get('item_code'), "parent": sales_order},'name')
 	if name:
 		data.update({"against_sales_order": sales_order, "so_detail" : name})
+	if not name:
 		return {}
 	return data
