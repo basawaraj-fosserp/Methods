45fa6eb90a2a70b8a24c925b49212cb9ecdf3846
viralkansodiya <viral@fosserp.com>
2025-04-26 05:50:24 +0000

    fixed the calculation issue

--- methods/public/js/bill_of_entry.js
+++ methods/public/js/bill_of_entry.js
@@ -1,65 +1,56 @@
 
 frappe.ui.form.on("Bill of Entry", {
     custom_exchange_rate : (frm)=>{
-        frm.trigger("calculate_assessable_value")        
-    },
-    calculate_assessable_value : (frm) =>{
-        let freight = frm.doc.freight || 0
-        let custom_exchange_rate = frm.doc.custom_exchange_rate || 0
-        let insurance = 0
-        if(flt(frm.doc.insurance)){
-            insurance = frm.doc.insurance/100 
-        }
-
-        frm.doc.items.forEach(e => {
-            assessable_value = (e.qty * e.rate * custom_exchange_rate) + freight + (e.qty * e.rate * custom_exchange_rate * insurance)
-            frappe.model.set_value(e.doctype, e.name, "assessable_value", assessable_value)
-            frappe.model.get_value("Item", e.item_code, "duty_and_charges", r=>{
-                if(r.duty_and_charges){
-                    console.log(r.duty_and_charges)
-                    let custom_duty  = assessable_value * r.duty_and_charges /100
-                    frappe.model.set_value(e.doctype, e.name, "customs_duty", custom_duty)
-                }
-            })
-            frm.refresh_field("items")
-        });
+        calculate_assessable_value(frm)        
     },
     freight : (frm) =>{
-        frm.trigger("calculate_assessable_value")
+        calculate_assessable_value(frm)
     },
     insurance : (frm) =>{
-        frm.trigger("calculate_assessable_value")
+        calculate_assessable_value(frm)
     },
     qty : (frm) =>{
-        frm.trigger("calculate_assessable_value")
+        calculate_assessable_value(frm)
     }
 })
 
 
 frappe.ui.form.on("Bill of Entry Item", {
     qty : (frm) =>{
-        console.log("hh")
-        frm.trigger("calculate_assessable_value")
+        calculate_assessable_value(frm)
     },
     calculate_assessable_value : (frm) =>{
-        let freight = frm.doc.freight || 0
-        let custom_exchange_rate = frm.doc.custom_exchange_rate || 0
-        let insurance = 0
-        if(flt(frm.doc.insurance)){
-            insurance = frm.doc.insurance/100 
-        }
-
-        frm.doc.items.forEach(e => {
-            console.log(r.duty_and_charges)
-            assessable_value = (e.qty * e.rate * custom_exchange_rate) + freight + (e.qty * e.rate * custom_exchange_rate * insurance)
-            frappe.model.set_value(e.doctype, e.name, "assessable_value", assessable_value)
-            frappe.model.get_value("Item", e.item_code, "duty_and_charges", r=>{
-                if(r.duty_and_charges){
-                    let custom_duty  = assessable_value * r.duty_and_charges /100
-                    frappe.model.set_value(e.doctype, e.name, "customs_duty", custom_duty)
-                }
-            })
-            frm.refresh_field("items")
-        });
+        
     },
 })
+
+function calculate_assessable_value(frm){
+    let freight = frm.doc.freight || 0
+    let custom_exchange_rate = frm.doc.custom_exchange_rate || 0
+    let insurance = 0
+
+    if(flt(frm.doc.insurance)){
+        insurance = frm.doc.insurance/100 
+    }
+
+    let total_assessable_value = 0
+
+    frm.doc.items.forEach(e => {
+        total_assessable_value += e.assessable_value
+    })
+
+    frm.doc.items.forEach(e => {
+        percentage_of_amount = e.assessable_value * 100 / total_assessable_value
+        devided_freight = percentage_of_amount * freight / 100 || 0
+        assessable_value = (e.qty * e.rate * custom_exchange_rate) + devided_freight + (e.qty * e.rate * custom_exchange_rate * insurance)
+        frappe.model.set_value(e.doctype, e.name, "assessable_value", assessable_value)
+
+        frappe.model.get_value("Item", e.item_code, "duty_and_charges", r=>{
+            if(r.duty_and_charges){
+                let custom_duty  = assessable_value * r.duty_and_charges /100
+                frappe.model.set_value(e.doctype, e.name, "customs_duty", custom_duty)
+            }
+        })
+        frm.refresh_field("items")
+    });
+}
