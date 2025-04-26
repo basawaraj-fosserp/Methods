54a1539c2fe6ab6abd073ed9c7e4422fb698dad2
viralkansodiya <viral@fosserp.com>
2025-04-26 05:14:30 +0000

    change the duty and charges

--- methods/public/js/bill_of_entry.js
+++ methods/public/js/bill_of_entry.js
@@ -15,7 +15,7 @@ frappe.ui.form.on("Bill of Entry", {
                 if(r.duty_and_charges){
                     console.log(r.duty_and_charges)
                     let custom_duty  = assessable_value * r.duty_and_charges /100
-                    frappe.model.set_value(e.doctype, e.name, "customs_duty", custom_duty)
+                    // frappe.model.set_value(e.doctype, e.name, "customs_duty", custom_duty)
                 }
             })
             frm.refresh_field("items")
@@ -50,7 +50,7 @@ frappe.ui.form.on("Bill of Entry Item", {
             frappe.model.get_value("Item", e.item_code, "duty_and_charges", r=>{
                 if(r.duty_and_charges){
                     let custom_duty  = assessable_value * r.duty_and_charges /100
-                    frappe.model.set_value(e.doctype, e.name, "customs_duty", custom_duty)
+                    // frappe.model.set_value(e.doctype, e.name, "customs_duty", custom_duty)
                 }
             })
             frm.refresh_field("items")
