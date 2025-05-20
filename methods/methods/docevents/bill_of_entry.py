18d8dd384abb7cdd98b5403edb0963df9e21dc8b
viralkansodiya <viral@fosserp.com>
2025-05-20 03:20:59 +0000

    override a whitelist method

--- methods/methods/docevents/bill_of_entry.py
+++ methods/methods/docevents/bill_of_entry.py
@@ -8,3 +8,54 @@ def check_quantity_overflow(self, method):
             if pi_data[0].get("total_qty"):
                 if pi_data[0].get("total_qty") + row.qty > total_qty:
                     frappe.throw(f"Row #{row.idx} : Bill of Entry already created for {pi_data[0].get('total_qty')} qty. Not allowed to create Bill of Entry greater then purchase invoice qty {total_qty}.")
+
+
+
+
+from india_compliance.gst_india.doctype.bill_of_entry.bill_of_entry import BillofEntry, set_missing_values
+
+class CustomBillofEntry(BillofEntry):
+    @frappe.whitelist()
+    def get_items_from_purchase_invoice(self, purchase_invoices):
+        frappe.has_permission("Bill Of Entry", "write")
+        frappe.has_permission("Purchase Invoice", "read")
+
+        existing_items = [
+            item.pi_detail for item in self.get("items") if item.pi_detail
+        ]
+        item_to_add = get_pi_items(purchase_invoices)
+
+        if not existing_items:
+            self.items = []
+
+        for item in item_to_add:
+            if item.pi_detail not in existing_items:
+                self.append("items", {**item})
+
+        set_missing_values(self)
+
+def get_pi_items(purchase_invoices):
+    pi_item = frappe.qb.DocType("Purchase Invoice Item")
+
+    return (
+        frappe.qb.from_(pi_item)
+        .select(
+            pi_item.item_code,
+            pi_item.item_name,
+            pi_item.parent.as_("purchase_invoice"),
+            pi_item.pending_boe_qty.as_("qty"),
+            pi_item.uom,
+            pi_item.qty,
+            pi_item.amount,
+            pi_item.cost_center,
+            pi_item.item_tax_template,
+            pi_item.gst_treatment,
+            pi_item.taxable_value.as_("assessable_value"),
+            pi_item.taxable_value,
+            pi_item.project,
+            pi_item.name.as_("pi_detail"),
+        )
+        .where(pi_item.parent.isin(purchase_invoices))
+        .where(pi_item.pending_boe_qty > 0)
+        .run(as_dict=True)
+    )
